03ac4fbf4759bff4defcc0823fd027ace43f1b3f
OGLOK <oguzgokyuzu01@gmail.com>
2022-04-21 00:48:09 +0300

    init

--- UDPClient.py
+++ UDPClient.py
@@ -8,19 +8,20 @@ ADDRESS = (IP, HOST)
 FORMAT = 'utf-8'
 
 
-json_message = {'type': 'SEND', 'body': {'method': 'GET', 'path': 'www.google.com', 'parameters': '/', 'Timeout': 1000}, 'Type': '‘AUTH’', 
-'token': '‘AAAAAAAAAAAAAAAAAAAAAMLheAAAAAAA0%2BuSeid%2BULvsea4JtiGRiSDSJSI%3DEUifiRBkKG5E2XzMDjRfl76ZC9Ub0wnz4XsNiRVBChTYbJcE3F’' }
-
-json_str = json.dumps(json_message)
-bytesToSend = str.encode(json_str)
+message = {'type': 'SEND', 'body': {'method': 'GET', 'path': 'www.google.com', 'parameters': '/', 'Timeout': 1}}, {'Type': 'AUTH', 'Body': {'token': 'og002098'}}
+json_message = json.dumps(message)
+json_bytes = str.encode(json_message)
 
 try:
     UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     UDPClientSocket.settimeout(5)
-    UDPClientSocket.sendto(bytesToSend, ADDRESS)
+    UDPClientSocket.sendto(json_bytes, ADDRESS)
+
     msgFromServer = UDPClientSocket.recvfrom(BUFFER_SIZE)
-    msg = "Message from Server: {}".format(msgFromServer[0].decode(FORMAT))
+        
+    msg = f"{(msgFromServer[0].decode(FORMAT))}"
     print(msg)
+
 except socket.timeout:
-    print("Client Timeout.")
+    print("Client couldn't connect to server in time.")
 
--- UDPServer.py
+++ UDPServer.py
@@ -1,8 +1,9 @@
+from cgi import print_form
 import http.client
 import json
 import socket
-from urllib import response
 
+from urllib import response
 from requests import request
 
 IP = socket.gethostbyname(socket.gethostname())
@@ -10,43 +11,63 @@ HOST = 5151
 BUFFER_SIZE = 1024
 ADDRESS = (IP, HOST)
 FORMAT = 'utf-8'
-json_error = { 'Success': True, 'Status': 405, 'Payload': { '“ERROR”': '“SEVER_TIMEOUT”', '“MESSAGE”': '“The server’s request timed out.”' } }
+
+authtoken = 'og00209'
+
+json_error = { 'Success': False, 'Status': 401, 'Payload': { 'ERROR': 'UNAUTHORIZED', 'MESSAGE': "Used an unauthorized token in the request." } }
 error = json.dumps(json_error)
 error_bytes = str.encode(error)
+
+authpass_json = {'success': True, 'status': 200, 'payload': { 'Message': 'Authentication successful' }}
+authpass = json.dumps(authpass_json)
+authpass_bytes = str.encode(authpass)
+
+bad_request_json = { 'success': False, 'status': 400, 'payload': { 'error': 'BAD_REQUEST', 'message': 'Incorrect properties in request'} } 
+bad_request = json.dumps(bad_request_json)
+bad_request_bytes = str.encode(bad_request)
+
+
+
 UDPServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDPServer.bind(ADDRESS)
-UDPServer.settimeout(10)
+UDPServer.settimeout(20)
 print("Server up and listening")
 
 try:
        while(True):
-        
+                
                 bytesAddressPair = UDPServer.recvfrom(BUFFER_SIZE)
                 message = bytesAddressPair[0].decode(FORMAT)
                 address = bytesAddressPair[1]
 
                 clientMsg = "Message from Client: {}".format(message)
                 clientIP  = "Client IP Address: {}".format(address)
-    
+
                 print(clientMsg)
                 print(clientIP)
 
                 my_json = json.loads(message)
-                connection = http.client.HTTPSConnection(my_json['body']['path'], timeout = my_json['body']['Timeout'])
-                try:
-                        connection.request(my_json['body']['method'], my_json['body']['parameters'])
-                except connection.timeout as st:
-                        print('406 – Request Timeout.')
+                connection = http.client.HTTPSConnection(my_json[0]['body']['path'], timeout = my_json[0]['body']['Timeout'])
+                        
+                if(my_json[1]['Body']['token'] == None):
+                        UDPServer.sendto(bad_request_bytes, address) 
+
+                elif(my_json[1]['Body']['token'] != authtoken):
                         UDPServer.sendto(error_bytes, address)
-                else:
+                        print("Authentication token is incorrect. Please use a correct token.")
+                        
+
+                elif(my_json[1]['Body']['token'] == authtoken):
+                        connection.request(my_json[0]['body']['method'], my_json[0]['body']['parameters'])
+
                         response = connection.getresponse()
                         print("Status: {} Reason: {}".format(response.status, response.reason))
                         strresponse = "Status: {} Reason: {}".format(response.status, response.reason)
-        
-                        bytesToSend = str.encode(strresponse)
 
-                        # Sending a reply to client
+                        bytesToSend = authpass_bytes + str.encode(strresponse) 
+
                         UDPServer.sendto(bytesToSend, address)
 
+                
 except socket.timeout:
         print("405 - Server Timeout.")
