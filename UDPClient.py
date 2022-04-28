6244bd9481a2e694083b7522d5da4f1843239c5f
OGL0K <oguzgokyuzu01@gmail.com>
2022-04-29 00:24:55 +0300

    init

--- UDPClient.py
+++ UDPClient.py
@@ -1,7 +1,10 @@
+from lib2to3.pgen2 import token
 import socket
 import json
 import string
 import random
+import textwrap
+from time import sleep
 
 letters_digits = string.ascii_lowercase + string.digits
 id = ''.join(random.choice(letters_digits) for i in range(8)) + "-"  +  ''.join(random.choice(letters_digits) for i in range(4)) + "-"  +  ''.join(random.choice(letters_digits) for i in range(4)) + "-"  +  ''.join(random.choice(letters_digits) for i in range(12))
@@ -12,28 +15,49 @@ BUFFER_SIZE = 1000000
 ADDRESS = (IP, HOST)
 FORMAT = 'utf-8'
 
-
-message = {'id': id ,'type': 'SEND', 'body': {'method': 'GET', 'path': 'https://www.google.com', 'queryParameters': '', 'body': {'username': 'og00209'}, 'Timeout': 1000}}, {'Type': 'AUTH', 'Body': {'token': ''}}
-json_message = json.dumps(message)
-json_bytes = str.encode(json_message)
-
-
 UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDPClientSocket.settimeout(5)
-
+UDPClientSocket.settimeout(50)
 
 try:
-    
-        def sendRequest():
-            UDPClientSocket.sendto(json_message.encode(FORMAT), ADDRESS)
+    type = input('Please enter your type: ')
+    authtoken = input('Please enter a token: ')
+except socket.timeout:
+    print('Timeout.')
 
-        def receiveResponse():
+send_request = {'id': id ,'type': 'SEND', 'body': {'method': 'GET', 'path': 'https://www.google.com', 'queryParameters': '', 'body': {'username': 'og00209'}, 'Timeout': 1000}},{'Type': 'AUTH', 'Body': {'token': 'og00209'}} 
+auth_request = {'Type': 'AUTH', 'Body': {'token': authtoken}}
+
+
+
+def sendRequest(send_request, UDPClientSocket):
+    
+    json_message = json.dumps(send_request)
+    packet_list = textwrap.wrap(json_message, 1024)
+    
+    try:
+       
+     
+        
+        for i in range(len(packet_list)):
+            packet = {"id": id, "packetNumber": i+1, "totalPackets": len(packet_list), "payloadData": packet_list[i] }
+            encodedpacket = json.dumps(packet).encode()
+            UDPClientSocket.sendto(encodedpacket, ADDRESS)
+            
+    except socket.timeout:
+        print('Timeout.')
+
+sendRequest(send_request, UDPClientSocket)
+
+def receiveRespond():
+    try:   
+        while(True):
+            
             ServerResponse = UDPClientSocket.recvfrom(BUFFER_SIZE)
             msg = f"{(ServerResponse[0].decode(FORMAT))}"
             print(msg)
-    
-        sendRequest()
-        receiveResponse()
+            sleep(2)
+            
+    except socket.timeout:
+        print('Timeout.')
 
-except socket.timeout:
-    print('Timeout.')
+receiveRespond()
--- UDPServer.py
+++ UDPServer.py
@@ -13,157 +13,173 @@ UDPServer.bind(ADDRESS)
 UDPServer.settimeout(20)
 print("Server is up and listening...")
 
+def handlePacket(request_json, address): 
+
+        global connectedClients
+        try:
+                if(request_json[0]['type'] == None or request_json[0]['body']['path'] == None or request_json[1]['Body']['token'] == None):
+                        return None
+                        
+        except KeyError:
+                bad_request_json = {'id': request_json[0]['id'], 'success': False, 'status': 400, 'payload': { 'error': 'BAD_REQUEST', 'message': 'You have made a bad request'}} 
+                bad_request = json.dumps(bad_request_json)
+                bad_request_bytes = str.encode(bad_request)
+                UDPServer.sendto(bad_request_bytes, address)
+                return None
+
+        if(request_json[1]['Body']['token'] == ''):
+
+                if address not in connectedClients:
+                        connectedClients[address] = {'requests': 0, 'authenticated': False}
+                        
+                connectedClients[address]['request'] += 1
+
+                print(connectedClients)
+                request = 10
+
+                if (request_json[0]['body']['method'] == 'GET'):
+                        try:
+
+                                r = requests.get(request_json[0]['body']['path'], timeout = request_json[0]['body']['Timeout'])
+                                
+                                if(r.status_code == requests.codes.ok):
+                                        success = True
+
+                                else:
+                                        success = False
+                                
+                                authpass_json = {'id': request_json[0]['id'], 'success': True, 'status': 200, 'payload': { 'Message': 'Authentication successful'}}
+                                authpass = json.dumps(authpass_json)
+                                authpass_bytes = str.encode(authpass)
+
+                                success_json = {'id': request_json[0]['id'] , 'success': success, 'status': r.status_code, 'payload': { 'content': str(r) ,'requests': request}}
+                                success_str = json.dumps(success_json) 
+                                success_bytes = str.encode(success_str)
+
+                                UDPServer.sendto(success_bytes, address)
+
+                                if(request == 0):
+                                        no_request_json = {'id': request_json[0]['id'], 'success': False, 'Status': 403, 'payload': { 'content': {'ERROR': 'UNAUTHORISED_REQUEST', 'message': 'You have reached your request limit' }}}
+                                        no_request_str = json.dumps(no_request_json)
+                                        no_request_bytes = str.encode(no_request_str)
+
+                                        UDPServer.sendto(no_request_bytes, address)
+                                
+
+
+                        except requests.exceptions.Timeout:
+                                timeout_json = {'id': request_json[0]['id'], 'status': 408,  'success': False, 'payload': { 'content': { 'error': 'TIMEOUT_ERROR', 'message': 'Your request has timed out.'}}}
+                                timeout_str = json.dumps(timeout_json)
+                                timeout_bytes = str.encode(timeout_str) 
+
+                                print('Timeout')
+                
+                
+
+        elif(request_json[1]['Body']['token'] != authtoken):
+                json_error = {'id': request_json[0]['id'], 'success': False, 'Status': 401, 'payload': { 'content': {'ERROR': 'UNAUTHORIZED', 'MESSAGE': 'Could not authenticate using your authentication token' }}}
+                error = json.dumps(json_error)
+                error_bytes = str.encode(error)
+                UDPServer.sendto(error_bytes, address)
+                print("Authentication token is incorrect. Please use a correct token.")
+        
+        
+        elif(request_json[1]['Body']['token'] == authtoken):
+
+
+                if (request_json[0]['body']['method'] == 'GET'):
+                        try:
+
+                                r = requests.get(request_json[0]['body']['path'], timeout = request_json[0]['body']['Timeout'])
+                                
+                                if(r.status_code == requests.codes.ok):
+                                        success = True
+
+                                else:
+                                        success = False
+                                
+                                authpass_json = {'id': request_json[0]['id'], 'success': True, 'status': 200, 'payload': { 'Message': 'Authentication successful'}}
+                                authpass = json.dumps(authpass_json)
+                                authpass_bytes = str.encode(authpass)
+
+                                success_json = {'id': request_json[0]['id'] , 'success': success, 'status': r.status_code, 'payload': { 'content': str(r) ,'requests': None }}
+                                success_str = json.dumps(success_json) 
+                                success_bytes = str.encode(success_str)
+
+                                UDPServer.sendto(authpass_bytes, address)
+                                UDPServer.sendto(success_bytes, address)
+                        
+                        
+
+                        except requests.exceptions.Timeout:
+                                timeout_json = {'id': request_json[0]['id'], 'status': 408,  'success': False, 'payload': { 'content': { 'error': 'TIMEOUT_ERROR', 'message': 'Your request has timed out.'}}}
+                                timeout_str = json.dumps(timeout_json)
+                                timeout_bytes = str.encode(timeout_str) 
+
+                                UDPServer.sendto(timeout_bytes, address)
+                                print('Timeout')
+                                
+
+                if (request_json[0]['body']['method'] == 'POST'):
+                        try:
+
+                                r = requests.post(request_json[0]['body']['path'], timeout = request_json[0]['body']['Timeout'])
+                                
+                                if(r.status_code == requests.codes.ok):
+                                        success = True
+
+                                else:
+                                        success = False
+                                
+                                authpass_json = {'id': request_json[0]['id'], 'success': True, 'status': 200, 'payload': { 'Message': 'Authentication successful'}}
+                                authpass = json.dumps(authpass_json)
+                                authpass_bytes = str.encode(authpass)
+
+                                success_json = {'id': request_json[0]['id'] , 'success': success, 'status': r.status_code, 'payload': { 'content': str(r) ,'requests': None }}
+                                success_str = json.dumps(success_json) 
+                                success_bytes = str.encode(success_str)
+
+                                UDPServer.sendto(success_bytes, address)
+
+                        except requests.exceptions.Timeout:
+                                timeout_json = {'id': request_json[0]['id'], 'status': 408,  'success': False, 'payload': { 'content': { 'error': 'TIMEOUT_ERROR', 'message': 'Your request has timed out.'}}}
+                                timeout_str = json.dumps(timeout_json)
+                                timeout_bytes = str.encode(timeout_str) 
+
+                                UDPServer.sendto(timeout_bytes, address)
+                                
+        
 try:         
         FORMAT = 'utf-8'
         authtoken = 'og00209'
-        connectedClients = {}
         
+        connectedClients = {} 
+        packets = {}
         while(True):
-                
+
                 bytesAddressPair = UDPServer.recvfrom(BUFFER_SIZE)
-                message = bytesAddressPair[0].decode(FORMAT)
+                packet = bytesAddressPair[0].decode(FORMAT)
                 address = bytesAddressPair[1]
+                request_json = json.loads(packet)
+                id = request_json['id']
 
-               
-                
-                connectedClients[address[0]] = 0
-                connectedClients[address[0]] += 1
+                if id not in packets:
+                        packets[id] = { request_json['packetNumber']: request_json['payloadData']}
+                else:
+                        packets[id]['packetNumber'] = request_json['payloadData']
 
-
-
-                clientMsg = "Request from Client: {}".format(message)
+                if len(packets[id]) == request_json['totalPackets']:
+                        reassembled = ''
+                        for i in range(len(packets[id])):
+                                reassembled += packets[id][i+1]
+                        print(f'REASSEMBLED PACKET: {reassembled}')
+                        handlePacket(json.loads(reassembled),address) 
                 clientIP  = "Client IP Address: {}".format(address)
                
-
                 print(clientIP)
-                print(clientMsg)
-                print(connectedClients)
-                request_json = json.loads(message)
-                        
-                try:
-                        if(request_json[0]['type'] == None or request_json[0]['body']['path'] == None or request_json[1]['Body']['token'] == None):
-                                continue
-                                     
-                except KeyError:
-                        bad_request_json = {'id': request_json[0]['id'], 'success': False, 'status': 400, 'payload': { 'error': 'BAD_REQUEST', 'message': 'You have made a bad request'}} 
-                        bad_request = json.dumps(bad_request_json)
-                        bad_request_bytes = str.encode(bad_request)
-                        UDPServer.sendto(bad_request_bytes, address)
-                        continue
-
-                if(request_json[1]['Body']['token'] == ''):
-                        
-                        request = 10
-
-                        if (request_json[0]['body']['method'] == 'GET'):
-                                try:
-
-                                        r = requests.get(request_json[0]['body']['path'], timeout = request_json[0]['body']['Timeout'])
-                                        
-                                        if(r.status_code == requests.codes.ok):
-                                                success = True
-
-                                        else:
-                                                success = False
-                                        
-                                        authpass_json = {'id': request_json[0]['id'], 'success': True, 'status': 200, 'payload': { 'Message': 'Authentication successful'}}
-                                        authpass = json.dumps(authpass_json)
-                                        authpass_bytes = str.encode(authpass)
-
-                                        success_json = {'id': request_json[0]['id'] , 'success': success, 'status': r.status_code, 'payload': { 'content': str(r) ,'requests': request}}
-                                        success_str = json.dumps(success_json) 
-                                        success_bytes = str.encode(success_str)
-
-                                        UDPServer.sendto(success_bytes, address)
-
-                                        if(request == 0):
-                                                no_request_json = {'id': request_json[0]['id'], 'success': False, 'Status': 403, 'payload': { 'content': {'ERROR': 'UNAUTHORISED_REQUEST', 'message': 'You have reached your request limit' }}}
-                                                no_request_str = json.dumps(no_request_json)
-                                                no_request_bytes = str.encode(no_request_str)
-
-                                                UDPServer.sendto(no_request_bytes, address)
-                                               
-
-
-                                except requests.exceptions.Timeout:
-                                        timeout_json = {'id': request_json[0]['id'], 'status': 408,  'success': False, 'payload': { 'content': { 'error': 'TIMEOUT_ERROR', 'message': 'Your request has timed out.'}}}
-                                        timeout_str = json.dumps(timeout_json)
-                                        timeout_bytes = str.encode(timeout_str) 
-
-                                        print('Timeout')
-                        
-                        
-
-                elif(request_json[1]['Body']['token'] != authtoken):
-                        json_error = {'id': request_json[0]['id'], 'success': False, 'Status': 401, 'payload': { 'content': {'ERROR': 'UNAUTHORIZED', 'MESSAGE': 'Could not authenticate using your authentication token' }}}
-                        error = json.dumps(json_error)
-                        error_bytes = str.encode(error)
-                        UDPServer.sendto(error_bytes, address)
-                        print("Authentication token is incorrect. Please use a correct token.")
-                
-                
-                elif(request_json[1]['Body']['token'] == authtoken):
-
-                        if (request_json[0]['body']['method'] == 'GET'):
-                                try:
-
-                                        r = requests.get(request_json[0]['body']['path'], timeout = request_json[0]['body']['Timeout'])
-                                        
-                                        if(r.status_code == requests.codes.ok):
-                                                success = True
-
-                                        else:
-                                                success = False
-                                        
-                                        authpass_json = {'id': request_json[0]['id'], 'success': True, 'status': 200, 'payload': { 'Message': 'Authentication successful'}}
-                                        authpass = json.dumps(authpass_json)
-                                        authpass_bytes = str.encode(authpass)
-
-                                        success_json = {'id': request_json[0]['id'] , 'success': success, 'status': r.status_code, 'payload': { 'content': str(r) ,'requests': None }}
-                                        success_str = json.dumps(success_json) 
-                                        success_bytes = str.encode(success_str)
-
-                                        UDPServer.sendto(success_bytes, address)
-                                
-                                
-
-                                except requests.exceptions.Timeout:
-                                        timeout_json = {'id': request_json[0]['id'], 'status': 408,  'success': False, 'payload': { 'content': { 'error': 'TIMEOUT_ERROR', 'message': 'Your request has timed out.'}}}
-                                        timeout_str = json.dumps(timeout_json)
-                                        timeout_bytes = str.encode(timeout_str) 
-
-                                        UDPServer.sendto(timeout_bytes, address)
-                                        print('Timeout')
-                                        
-
-                        if (request_json[0]['body']['method'] == 'POST'):
-                                try:
-
-                                        r = requests.post(request_json[0]['body']['path'], timeout = request_json[0]['body']['Timeout'])
-                                        
-                                        if(r.status_code == requests.codes.ok):
-                                                success = True
-
-                                        else:
-                                                success = False
-                                        
-                                        authpass_json = {'id': request_json[0]['id'], 'success': True, 'status': 200, 'payload': { 'Message': 'Authentication successful'}}
-                                        authpass = json.dumps(authpass_json)
-                                        authpass_bytes = str.encode(authpass)
-
-                                        success_json = {'id': request_json[0]['id'] , 'success': success, 'status': r.status_code, 'payload': { 'content': str(r) ,'requests': None }}
-                                        success_str = json.dumps(success_json) 
-                                        success_bytes = str.encode(success_str)
-
-                                        UDPServer.sendto(success_bytes, address)
-
-                                except requests.exceptions.Timeout:
-                                        timeout_json = {'id': request_json[0]['id'], 'status': 408,  'success': False, 'payload': { 'content': { 'error': 'TIMEOUT_ERROR', 'message': 'Your request has timed out.'}}}
-                                        timeout_str = json.dumps(timeout_json)
-                                        timeout_bytes = str.encode(timeout_str) 
-
-                                        UDPServer.sendto(timeout_bytes, address)
-                                             
+                               
+       
+        
 except socket.timeout:
         print("405 - Server Timeout.")
 
